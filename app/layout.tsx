a7e12229996fa31592e75c8fd3de70dcd941d520
levywanke <levywanke@gmail.com>
2025-03-21 15:12:58 +0300

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'NFT',
+  description: 'non fungible tokens',
+  creator: 'levy',
 }
 
 export default function RootLayout({
